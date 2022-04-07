d2492862b96ac8ea81c4be89533197a492a8883b
aahmedyusuf <aahmedyusuf2@student.gsu.edu>
2022-04-06 22:43:45 -0400

    cleaned the code

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -10,15 +10,12 @@ function Home_Page() {
   return (
       <div className = "Parent">
         <div className = "Account-Info">
-          <div className = "Logo">
-          <h1> 🌱 </h1>
-          </div>
           <center>
-          <h1> Welcome to Seedling. </h1>
-          <h2> All your links in one place! 🌱 </h2>
+          <h1> Welcome to Trade-Inc </h1>
+          <h2> A place where small business can do business with manufacturer. </h2>
 
           <div className = "Login-Form">
-          <h4>Log in to your Seedling account</h4>
+          <h4>Log in to your Trade-inc account</h4>
           <input className = "Field" type="text" id="fusername" name="fusername" placeholder = "Username"/>
           <input className = "Field" type="password" id="fpassword" name="fpassword" placeholder = "Password"/>
           
