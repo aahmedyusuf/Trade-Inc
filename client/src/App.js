813613808dab0bac7980436b2a14c9a33507fe52
f-hash <abimbolaakosile922@gmail.com>
2022-04-10 18:05:07 -0400

    Created checkout page and thank you page and added both pages to router

--- client/src/App.js
+++ client/src/App.js
@@ -4,33 +4,40 @@ import './App.css';
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Attentition_Page from './Pages/Attentition_Page';
+import Checkout from './Pages/Checkout';
 import Home from './Pages/Home';
+import Thank_You from './Pages/Thank_You';
 import Manufacturer from './Pages/Manufacturer';
 import CreateProduct from './Pages/CreateProduct';
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/">
-        <Route index element={<Attentition_Page />} />
-      </Route>
-      <Route path="/home">
-      <Route index element={<Home />} />
-      </Route>
-      <Route path="/checkout">
+      <Routes>
+        <Route path="/">
+          <Route index element={<Attentition_Page />} />
+        </Route>
+        <Route path="/home">
+          <Route index element={<Home />} />
+        </Route>
+        <Route path="/Checkout">
+          <Route index element={<Checkout />} />
 
-      </Route>
-      <Route path="/product">
-      </Route>
-      <Route path="/Manufacturer">
-      <Route index element={<Manufacturer />} />
-      </Route>
-      <Route path="/CreateProduct">
-      <Route index element={<CreateProduct />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>  );
+        </Route>
+        <Route path="/Thank_You">
+          <Route index element={<Thank_You />} />
+
+        </Route>
+        <Route path="/product">
+        </Route>
+        <Route path="/Manufacturer">
+          <Route index element={<Manufacturer />} />
+        </Route>
+        <Route path="/CreateProduct">
+          <Route index element={<CreateProduct />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>);
 }
 
 export default App;
---
+++ client/src/Pages/Thank_You.js
@@ -0,0 +1,8 @@
+import React from 'react'
+function thankyou() {
+    return (
+        <h2>Thank you for your order!</h2>
+    )
+}
+export default thankyou;
+
